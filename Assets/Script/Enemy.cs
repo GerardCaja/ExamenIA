c9d37edbc58c14ee8afe51dd23d8b18599923fb8
GerardCaja <120013160+GerardCaja@users.noreply.github.com>
2024-03-01 09:28:22 +0100

    asdas

---
+++ Assets/Script/Enemy.cs
@@ -0,0 +1,108 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+    /*public enum State
+    {
+        Patrolling,
+        Chasing,
+        Attacking
+    }
+
+    public State currentState;
+
+    private UnityEngine.AI.NavMeshAgent agent;
+
+    public Transform player;
+
+    [SerializeField] private Transform[] patrolPoints;
+    [SerializeField] private float detectionRange = 15;
+    [SerializeField] private float attackingRange = 5;
+
+    void Awake()
+    {
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+    }
+
+    void Start()
+    {
+        SetRandomPoint();
+
+        currentState = State.Patrolling;
+    }
+
+    void Update()
+    {
+        switch (currentState)
+        {
+            case State.Patrolling:
+                Patrol();
+            break;
+
+            case State.Chasing:
+                Chase();
+            break;
+
+            case State.Attacking:
+                Attack();
+            break;
+        }
+    }
+
+    void Patrol()
+    {
+        if(IsInRange(detectionRange) == true)
+        {
+            currentState = State.Chasing;
+        }
+
+        if(agent.remainingDistance < 0.5f)
+        {
+            SetRandomPoint();
+        }
+    }
+
+    void Chasing()
+    {
+        if(IsInRange(detectionRange) == false)
+        {
+            SetRandomPoint();
+
+            currentState = State.Patrolling;
+        }
+
+        if(IsInRange(attackingRange) == true)
+        {
+            currentState = State.Attacking;
+        }
+
+        agent.destination = player.position;
+    }
+
+    void Attack()
+    {
+        DeBug.Log("Atacando");
+
+        currentState = State.Chasing;
+    }
+
+    void SetRandomPoint()
+    {
+        agent.destination = patrolPoints[Random.Range(0, 4)].position;
+    }
+
+    bool IsInRange(float range)
+    {
+        if(Vector3.Distance(transform.position, player.position) < range)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }*/
+}
